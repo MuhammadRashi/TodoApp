c59b3ea70ddead446755470ed7f966beafc356fd
MuhammadRashi <rashid.kollam@gmail.com>
2023-05-16 22:17:45 +0530

    Initial commit

---
+++ TodoApp/src/components/Todo.jsx
@@ -0,0 +1,15 @@
+import React, { Fragment } from 'react'
+import './Todo.css'
+
+export const Todo = () => {
+  return (
+    <Fragment>
+
+        <div className='todo-container' >
+            {/* <div className='head-container'> */}
+                <h1>Todo List</h1>
+            {/* </div> */}
+        </div>
+    </Fragment>
+  )
+}
